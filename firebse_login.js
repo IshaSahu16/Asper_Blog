5dfcf06eb077e8c067bedf9c44da75d1aefb5f1a
Ishaan <ishaanmathur778@gmail.com>
2023-07-09 09:34:47 +0530

    Added firebase to login page

---
+++ firebse_login.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA96RtouYomgFfcOnO-r30xSdHOWm1uYaE",
+    authDomain: "asperia-2453d.firebaseapp.com",
+    projectId: "asperia-2453d",
+    storageBucket: "asperia-2453d.appspot.com",
+    messagingSenderId: "1040391468706",
+    appId: "1:1040391468706:web:a56ed4d30a0b12542c09fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+    e.preventDefault();
+    var obj = {
+    email: email.value,
+    password: password.value,
+    };
+
+    signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+        alert("logged in Successfully")
+        var aaaa =  (success.user.uid);
+        localStorage.setItem("uid",aaaa)
+        console.log(aaaa)
+        
+        
+        
+        window.location.replace(index.html)
+        // localStorage.setItem(success,user,uid)
+        
+    })
+    .catch(function (err) {
+        alert("login error"+err);
+    });
+
+    console.log(obj);
+    }
